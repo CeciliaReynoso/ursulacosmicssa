56acaaa686153a9156594fc4195ad9fbc9c80aef
ceciliareynoso <cecilia.reynosom@outlook.com>
2024-05-20 15:29:18 -0500

    Prueba completa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-const root = document.querySelector('#root')
-const toggler = document.querySelector('#toggler')
-
-toggler.addEventListener('click', ()=> {
-    root.classList.toggle('open')
-})
+document.addEventListener("DOMContentLoaded", function() {
+    // Comprobar si hay un hash en la URL (un enlace con #producto1, #producto2, etc.)
+    if(window.location.hash) {
+        // Obtenemos el ID del producto desde el hash
+        var productId = window.location.hash.substring(1);
+        // Intentamos encontrar el elemento con el ID del producto
+        var productElement = document.getElementById(productId);
+        // Si lo encontramos, lo desplazamos a la parte superior de la ventana
+        if(productElement) {
+            productElement.scrollIntoView();
+        }
+    }
+});
